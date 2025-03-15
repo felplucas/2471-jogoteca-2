86ce04644ae7819a89f571469dfd169b65d762f2
Lucas Felipe Patera <lucasfelipepatera@gmail.com>
2025-03-15 17:38:41 -0300

    mysql_&_requirements
    Escondi as credencias do banco de dados MYSql e gerei o arquivo requirements.txt do projeto, para atender as boas práticas

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 SECRET_KEY = 'alura'
 
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'mysql+mysqlconnector',
-        usuario = 'root',
-        senha = 'admin',
-        servidor = 'localhost',
-        database = 'jogoteca'
+        SGBD='mysql+mysqlconnector',
+        usuario=os.getenv('DB_USER'),
+        senha=os.getenv('DB_PASS'),
+        servidor=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME')
     )
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
--- prepara_banco.py
+++ prepara_banco.py
@@ -1,13 +1,22 @@
-import mysql.connector
+from dotenv import load_dotenv
 from mysql.connector import errorcode
 from flask_bcrypt import generate_password_hash
+import mysql.connector
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
 
 print("Conectando...")
 try:
       conn = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='admin'
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS
       )
 except mysql.connector.Error as err:
       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
